b70def5dec73c0442cce6578b41e8da5cb3786b6
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-02 14:53:04 +0100

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -2,6 +2,8 @@
    home.shellAliases = {
     fleeks = "cd /Users/alex/.local/share/fleek";
     
+    pinentry-mac = "~/.nix-profile/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac";
+    
     # bat --plain for unformatted cat
     catp = "bat -P";
     
