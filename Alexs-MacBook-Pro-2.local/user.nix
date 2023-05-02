73660af506dedbbcf0854c5f706078616b7cda42
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-02 16:27:49 +0100

    fleek: apply

--- Alexs-MacBook-Pro-2.local/user.nix
+++ Alexs-MacBook-Pro-2.local/user.nix
@@ -5,7 +5,7 @@
   enableAutosuggestions = true;
   completionInit = [
       "autoload -Uz compinit && compinit"
-    , "zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'"
+    "zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'"
   ]
   
 }
