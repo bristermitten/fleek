6dabcb316165d2e5a706c8f14f2cbdc8b7f43e2d
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-30 14:28:13 +0100

    zsh improvements

--- shell.nix
+++ shell.nix
@@ -10,11 +10,31 @@
     theme = "TwoDark";
   };
   # zsh
-  programs.zsh.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enable = true;
+  programs.zsh = {
+    profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
 
-  programs.zsh.initExtra = ''
-    export XDG_RUNTIME_DIR="$TMPDIR"
-  '';
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    autocd = true;
+
+
+    initExtra = ''
+      export XDG_RUNTIME_DIR="$TMPDIR"
+    '';
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+  };
 }
