3c55b626ef4ad43247c5d5a689cde4aab24c03d7
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-07-09 20:01:06 +0100

    add fix-nix

--- aliases.nix
+++ aliases.nix
@@ -9,5 +9,8 @@
 
     # replace cat with bat
     cat = "bat";
+
+    # Easy alias to fix whenever nix fucks itself up, see https://github.com/NixOS/nix/issues/2899#issuecomment-699299567 for source
+    fix-nix = "sudo launchctl setenv NIX_SSL_CERT_FILE \"/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt\" && sudo launchctl kickstart -k system/org.nixos.nix-daemon";
   };
 }
