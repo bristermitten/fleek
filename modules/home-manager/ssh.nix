c010014a6f956d17003d915ad804f25e6c1e9698
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-11-20 17:16:52 +0000

    uob lab ssh

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -10,6 +10,16 @@
       Host github-uni
               HostName github.com
               IdentityFile ~/.ssh/id_ed25519_uni
+
+      Host lab
+              HostName rd-mvb-linuxlab.bristol.ac.uk
+              ProxyJump seis
+
+      Host seis
+            HostName seis.bris.ac.uk
+            UserName fi22264
+
+
     '';
   };
 }
